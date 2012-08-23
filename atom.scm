fd474c4ff4b12bb16a4b99882ee9d37fc2c4aad1
Mathias Huber <mhuber@linux-magazin.de>
2012-08-23 15:52:36 +0200

    helper function equan?

--- atom.scm
+++ atom.scm
@@ -122,6 +122,17 @@
       ((atom? (car l)) (car l))
       (else (leftmost (car l))))))
                 
-      
+;; p. 91
+
+; helper functions
+
+(define equan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1)(number? a2))
+       (= a1 a2))
+      ((or (number? a1)(number? a2))
+       #f)
+      (else (eq? a1 a2)))))
       
       
